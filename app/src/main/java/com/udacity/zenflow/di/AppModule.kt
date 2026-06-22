1e8fd373d63feb49c9e86841f855e3d196c3b48f
Federico Martini <Martini.f@gmail.com>
2026-06-22 10:31:11 +0200

    Implement Room database and JournalDao providers in AppModule

--- app/src/main/java/com/udacity/zenflow/di/AppModule.kt
+++ app/src/main/java/com/udacity/zenflow/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.udacity.zenflow.di
 
+import androidx.room.Room
 import android.content.Context
 import com.udacity.zenflow.data.JournalDao
 import com.udacity.zenflow.data.JournalRepository
@@ -24,7 +25,6 @@ abstract class AppModule {
         impl: SystemTimeProvider
     ): TimeProvider
 
-    // TODO: This connects your Interface (JournalRepository) to your Implementation (RoomJournalRepository).
     @Binds
     @Singleton
     abstract fun bindJournalRepository(
@@ -33,21 +33,21 @@ abstract class AppModule {
 
     companion object {
 
-        // TODO: Instantiate the Room Database.
-        // 1. Use Room.databaseBuilder.
-        // 2. Decide on a database name.
-        // 3. For development, you might want to allow destructive migration to avoid crashes on schema changes.
         @Provides
         @Singleton
         fun provideDatabase(@ApplicationContext context: Context): ZenFlowDatabase {
-            TODO("Not yet implemented")
+            return Room.databaseBuilder(
+                context,
+                ZenFlowDatabase::class.java,
+                "zen_flow_database"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
         }
 
-        // TODO: Provide the JournalDao dependency.
-        // Hint: You don't create DAOs directly; you ask the database instance for them.
         @Provides
         fun provideJournalDao(database: ZenFlowDatabase): JournalDao {
-            TODO("Not yet implemented")
+            return database.journalDao()
         }
     }
 }
